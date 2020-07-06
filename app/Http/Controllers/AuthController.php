6e9f199d19f4d09df1afe78676ccf89227da7d94
Kushanrawindu <kushanrawindu@gmail.com>
2020-07-06 15:19:09 +0530

    confirm account(activate user)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -82,6 +82,23 @@ class AuthController extends Controller
         ]);
     }
 
+    public function signupActivate($token)
+    {
+        $user = User::where('activation_token', $token)->first();
+
+        if(!$user){
+            return response()->json([
+                'message' => 'This activation token is invalid.'
+            ], 404);
+        }
+
+        $user->active = true;
+        $user->activation_token = '';
+        $user->save();
+
+        return $user;
+    }
+
     /**
      * Logout user (Revoke the token)
      *
